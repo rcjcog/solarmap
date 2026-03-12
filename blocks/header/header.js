b19bd3ac3392ae12668d3e5bdf4c66556c9b732d
Joshi <2469863@cognizant.com>
2026-03-12 10:24:48 +0530

    top header

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -117,7 +117,7 @@ if (navTopFragment) {
   nav.id = 'nav';
   while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
 
-  const classes = ['brand', 'sections', 'tools'];
+  const classes = ['navtop','brand', 'sections', 'tools'];
   classes.forEach((c, i) => {
     const section = nav.children[i];
     if (section) section.classList.add(`nav-${c}`);
